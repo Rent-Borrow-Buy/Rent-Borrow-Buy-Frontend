8ddf78dfbca6dc5ac478631ca1739b0265ab25d4
Cole Rossman <colerossman99@gmail.com>
2022-07-12 15:55:47 -0700

    signIn fetch function created in services/users

--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,4 @@
 export const signUp = async ({ email, password }) => {
-  console.log('email', email);
-  console.log('password', password);
   const resp = await fetch(`${process.env.API_URL}/api/v1/users`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json'},
@@ -10,6 +8,20 @@ export const signUp = async ({ email, password }) => {
   });
 
   if (!resp.ok) throw new Error('Invalid email or password');
+
+  return resp.json();
+}
+
+export const signIn = async ({ email, password}) => {
+  const resp = await fetch(`${process.env.API_URL}/api/v1/users/sessions`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    credentials: 'include',
+    mode: 'cors',
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!resp.ok) throw new Error('Invalid email or password')
 
   return resp.json();
 }
