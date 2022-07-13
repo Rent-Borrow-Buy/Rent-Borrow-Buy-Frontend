bdf64d6c4051ea88b63267e2fc492898463d1ca1
m-ghiringhelli <mghiring@gmail.com>
2022-07-13 10:26:24 -0700

    added signout service to logout user

--- src/services/users.js
+++ src/services/users.js
@@ -13,7 +13,6 @@ export const signUp = async ({ email, password }) => {
 }
 
 export const signIn = async ({ email, password}) => {
-  console.log('sign in');
   const resp = await fetch(`${process.env.API_URL}/api/v1/users/sessions`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json'},
@@ -25,4 +24,17 @@ export const signIn = async ({ email, password}) => {
   if (!resp.ok) throw new Error('Invalid email or password')
 
   return resp.json();
+}
+
+export const signOut = async () => {
+  const resp = await fetch(`${process.env.API_URL}/api/v1/users/sessions`, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json'},
+    credentials: 'include',
+    mode: 'cors',
+  });
+
+  if (!resp.ok) throw new Error('There was a problem signing out');
+
+  return (resp.json);
 }
--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AuthForm from '../components/AuthForm'
+import { signOut } from '../services/users'
 
 export default function Auth() {
   return (
     <>
       <AuthForm />
+      <button onClick={() => signOut()}>log out</button>
     </>
   )
 }
