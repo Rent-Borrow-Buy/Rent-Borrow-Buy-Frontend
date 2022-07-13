296ff414773d72af4f973395c1566df092655273
Cole Rossman <colerossman99@gmail.com>
2022-07-13 11:14:39 -0700

    userContext created and holds user state

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,18 @@
 import { createContext, useState } from "react";
+import { getUser } from "../services/users";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+
+    const currentUser = getUser();
+
+    const [error, setError] = useState('');
+    const [user, setUser] = useState(currentUser || {email: null});
+
+    return (
+        <UserContext.Provider value={{ error, setError, user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    );
+}
