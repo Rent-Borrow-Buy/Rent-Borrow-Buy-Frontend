7978533782173e1013b148962e946e7963849313
Cole Rossman <colerossman99@gmail.com>
2022-07-19 11:22:29 -0700

    PrivateRoute component created

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { Redirect, Route, useLocation } from "react-router-dom";
+import { useAuth } from "../../hooks/user";
+
+export default function PrivateRoute({ children, ...rest }) {
+    const { user } = useAuth();
+    const location = useLocation();
+
+  return (
+    <Route {...rest}>
+        {user.email ? (children) : (
+            <Redirect to={{ 
+                pathname: '/auth',
+                state: { origin: location },
+            }}
+            />
+        )}
+    </Route>
+  );
+}
