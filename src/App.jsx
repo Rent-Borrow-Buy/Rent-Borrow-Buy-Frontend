7c569e0951e44db0a3171d10bd0b52d4ce25f525
Andrea Cleland <andreaclelan@gmail.com>
2022-07-18 18:10:58 -0700

    changed route in App.jsx to be RESTful and created UpdateItem form, but it needs work

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import { Route, Switch } from "react-router-dom";
-import { useAuth } from "./hooks/user";
-import AddItem from "./views/AddItem";
-import Auth from "./views/Auth";
-import Home from "./views/Home";
-import UpdateItem from "./views/UpdateItem";
+import { Route, Switch } from 'react-router-dom';
+import { useAuth } from './hooks/user';
+import AddItem from './views/AddItem';
+import Auth from './views/Auth';
+import Home from './views/Home';
+import UpdateItem from './views/UpdateItem';
 
 export default function App() {
-
-  const {loading} = useAuth();
-  if(loading) return <div>loading</div>
+  const { loading } = useAuth();
+  if (loading) return <div>loading</div>;
   return (
     <>
       <Switch>
-        <Route path='/items/:id/edit'>
-          <UpdateItem/>
+        <Route path="/items/:id/edit">
+          <UpdateItem />
         </Route>
-        <Route path="/additem">
+        <Route path="/items/new">
           <AddItem />
         </Route>
         <Route path="/auth">
           <Auth />
         </Route>
-        <Route path="/"> 
+        <Route path="/">
           <Home />
         </Route>
       </Switch>
     </>
-  )
+  );
 }
