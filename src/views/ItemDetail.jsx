f408a53df71ef36b90cf30f360f1e2552282ca09
Cole Rossman <colerossman99@gmail.com>
2022-07-20 10:34:01 -0700

    useEffect in itemdetail view fetches item by id

--- src/views/ItemDetail.jsx
+++ src/views/ItemDetail.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { useAuth } from '../hooks/user';
+import { getItemById } from '../services/items';
 
 
 export default function ItemDetail() {
     const [item, setItem] = useState({});
 
     const { user, errorMessage, setErrorMessage, loading, setLoading } = useAuth();
-
+    const { id } = useParams();
     const isCreator = user.id === item.user_id;
+
+    useEffect(() => {
+     try {
+        const fetchData = async () => {
+            const item = await getItemById(id);
+            setItem(item);
+        }
+        fetchData();
+     } catch (e) {
+       setErrorMessage(e.message); 
+     }
+    }, [id]);
 
   return (
     <div>ItemDetail</div>
